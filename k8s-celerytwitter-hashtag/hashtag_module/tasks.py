553b7412f2170f42f8f4a711126da6eb6ed23397
YukyCookie <20656082+YukyCookie@users.noreply.github.com>
2023-01-05 04:54:09 +0100

    Add files via upload

--- k8s-celerytwitter-hashtag/hashtag_module/tasks.py
+++ k8s-celerytwitter-hashtag/hashtag_module/tasks.py
@@ -1,8 +1,8 @@
 from hashtag_module.celery import celery_app
-# from celery import CELERY_BROKER_URL, CELERY_RESULT_BACKEND, BEARER_TOKEN
 import hashtag_module.celery as celeryconfig
 import tweepy
 from textblob import TextBlob
+from wordcloud import WordCloud,STOPWORDS
 
 client = tweepy.Client(bearer_token = celeryconfig.BEARER_TOKEN)
 
@@ -12,15 +12,23 @@ def insert_data(msg):
 
 @celery_app.task(queue='hash_queue')
 def search(query, limit):
+    STOPWORDS.update(['https', query, 't', 'co', 'will', 'amp'])
     tweets = tweepy.Paginator(client.search_recent_tweets, query="#{} -is:retweet lang:en".format(query), max_results=100).flatten(limit)
     all_tweets = []
+    text = ""
     for tweet in tweets:
         t = dict(tweet)
+        text = text + " " + t["text"]
         polarity = TextBlob(t["text"]).sentiment.polarity
         subjectivity = TextBlob(t["text"]).sentiment.subjectivity
         insert_data.delay(t)
         all_tweets.append([t["text"],polarity,subjectivity])
+    wc = WordCloud(background_color="white", repeat=False ,stopwords=STOPWORDS, height=350, width=1000, min_font_size=10, min_word_length=3).generate(text)
+    wc_svg = wc.to_svg(embed_font=True)
+    all_tweets.append("""{}""".format(wc_svg))
+    
     return all_tweets
+
 
 
 
